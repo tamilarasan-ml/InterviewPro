4cdf492d73b6f5d5357f33dd41f464ac371c2acf
tamilarasan-ml <tamilarasanpkv@outlook.com>
2026-08-02 17:53:18 +0530

    IPAI-TD-01: Standardize shared types and project structure

--- src/auth/AuthContext.tsx
+++ src/auth/AuthContext.tsx
@@ -1,5 +1,5 @@
 import { createContext } from "react";
-import type { AuthContextType } from "./auth.types";
+import type { AuthContextType } from "../types/auth.types";
 
 export const AuthContext =
   createContext<AuthContextType | undefined>(
