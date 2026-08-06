9cff9c186298859ebd6d03d975f0a7a17139ba8e
tamilarasan-ml <tamilarasanpkv@outlook.com>
2026-08-06 06:40:27 +0530

    fix(IPAI-96): recover frontend architecture and restore successful build

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,9 +1,14 @@
-export { api } from "./api";
-export { default as axiosInstance } from "./axios";
+import axiosInstance from "./axios";
+
+export const api = axiosInstance;
+
 export { ENDPOINTS } from "./endpoints";
 
 export { AuthService } from "./auth.service";
 export { UserService } from "./user.service";
+export { ProfileService } from "./profile.service";
+export { LearningService } from "./learning.service";
+export { PracticeService } from "./practice.service";
+
 export { InterviewService } from "./interview.service";
-export * from "./profile.service";
-export * from "./learning.service";
+export { AIInterviewService } from "./aiInterview.service";
